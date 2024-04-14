c09444af4a2906ddb034b6847687a41388064e22
Nikita Nagin <email@example.com>
2024-04-14 15:47:18 +0300

    - add static swagger for pt_app

--- pt_app/apps/train_api/server.py
+++ pt_app/apps/train_api/server.py
@@ -3,10 +3,18 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
+from fastapi.staticfiles import StaticFiles
 from apps.train_api.src.routers import train_router
-
-app = FastAPI(redoc_url=None)
+from fastapi.openapi.docs import (
+    get_swagger_ui_html,
+)
+from fastapi.openapi.utils import get_openapi
+# app = FastAPI(redoc_url=None)
+app = FastAPI(docs_url=None,
+              redoc_url=None,
+              openapi_url="/docs/openapi.json")
+static_path = "/".join(os.getcwd().split('/')[:len(os.getcwd().split('/'))-2])
+app.mount(static_path, StaticFiles(directory=static_path), name="static")
 
 """ Настройки CORS """
 
@@ -24,6 +32,38 @@ app.add_middleware(
 
 app.include_router(train_router)
 
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="S3 REPL service",
+        version="1.0.0",
+        description="replication service",
+        routes=app.routes,
+    )
+    # openapi_schema["info"]["x-logo"] = {
+    #     "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    # }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        # openapi_url="/url_root/openapi.json",
+        title="App1 api",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url=f"{static_path}/static/swagger/swagger-ui-bundle.js",  # Optional
+        swagger_css_url=f"{static_path}/static/swagger/swagger-ui.css",  # Optional
+        # swagger_favicon_url="/static/swagger/favicon-32x32.png",  # Optional
+        swagger_favicon_url=f"{static_path}/static/swagger/innovation.png",  # Optional
+        # custom_js_url="/static/custom_script.js",
+    )
 
 if __name__ == '__main__':
     uvicorn.run(app=app, host=os.environ.get('TRAIN_API_HOST'), port=int(os.environ.get('TRAIN_API_PORT')))
