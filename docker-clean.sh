a806fc1a3f52f717bec8ff8245b776028297b6dd
Nikita Nagin <email@example.com>
2024-05-08 02:17:49 +0300

    - fix

--- docker-clean.sh
+++ docker-clean.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-#docker compose down -v || true
 docker stop `docker ps -qa` || true
-docker rm `docker ps -qa` || true
-#docker rmi -f $(docker images -aq) || true
-docker rmi -f `docker images -qa` || true
+docker rmi -f $(docker images -aq) || true
 docker volume rm $(docker volume ls -qf) || true
 docker network rm `docker network ls -q` || true
 docker system prune -a | printf 'y' || true
