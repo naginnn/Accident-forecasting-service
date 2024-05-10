ea020b36fd98c5f50e2c96b2b8598417d20cfb9e
Nikita Nagin <email@example.com>
2024-05-10 17:25:35 +0300

    - fix path

--- pt_app/apps/train_api/server.py
+++ pt_app/apps/train_api/server.py
@@ -72,6 +72,6 @@ async def custom_swagger_ui_html():
 if __name__ == '__main__':
     import pandas as pd
     files = {}
-    files["test.xlsx"] = pd.ExcelFile(f"{os.getcwd()}/test.xlsx", )
+    files["test.xlsx"] = pd.ExcelFile(f"{os.getcwd()}/autostart/test.xlsx", )
     prepare_dataset(files=files)
     uvicorn.run(app=app, host=os.environ.get('TRAIN_API_HOST'), port=int(os.environ.get('TRAIN_API_PORT')))
