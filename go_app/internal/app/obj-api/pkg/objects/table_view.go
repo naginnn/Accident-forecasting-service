07454855131c3d80eeb0ebfd8779d847ac8820d2
Nikita Nagin <email@example.com>
2024-05-22 21:25:15 +0300

    - add transform model and predict tables - add new table event_types - next step: save_predict events

--- go_app/internal/app/obj-api/pkg/objects/table_view.go
+++ go_app/internal/app/obj-api/pkg/objects/table_view.go
@@ -35,6 +35,7 @@ func (h handler) GetTableView(c *gin.Context) {
 		Probability float64 `json:"probability"`
 		IsApproved  bool    `json:"is_approved"`
 		IsClosed    bool    `json:"is_closed"`
+		IsWarning   bool    `json:"is_warning"`
 	}
 
 	q := `select
@@ -43,7 +44,8 @@ func (h handler) GetTableView(c *gin.Context) {
     ld.id location_district_consumer_id, ld.name location_district_consumer_name,
     la.id location_area_consumer_id, la.name location_area_consumer_name, la.coordinates location_area_consumer_coordinates,
     c.id consumer_id, c.name consumer_name, c.address consumer_address, c.coordinates consumer_coordinates,
-    ecf.source, ecf.description, ecf.probability, ecf.is_approved, ecf.is_closed
+    ecf.source, ecf.description, ecf.probability, ecf.is_approved, ecf.is_closed,
+    (SELECT EXISTS(SELECT true FROM public.event_consumers WHERE id = ec.id and is_closed = false)) as is_warning
 from obj_consumers as c
          join public.location_districts ld on ld.id = c.location_district_id
          join public.location_areas la on ld.id = c.location_area_id
@@ -51,8 +53,8 @@ from obj_consumers as c
          join public.obj_source_consumer_stations scs on cs.id = scs.obj_consumer_station_id
          join public.obj_source_stations ss on ss.id = scs.obj_source_station_id
          left join (select ecc.obj_consumer_id, max(ecc.id) as id from public.event_consumers as ecc group by obj_consumer_id) ec on ec.obj_consumer_id = cs.id
-         left join public.event_consumers ecf on ecf.id = ec.id and ecf.is_closed = false
-order by ecf.is_closed`
+         left join public.event_consumers ecf on ecf.id = ec.id
+order by is_warning desc;`
 
 	fmt.Println(q)
 
