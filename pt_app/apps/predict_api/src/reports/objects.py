116ec0fed1eb5678a15221becd37b278f409844b
lestar <gamser11@yandex.ru>
2024-06-14 23:39:57 +0300

    update object report

--- pt_app/apps/predict_api/src/reports/objects.py
+++ pt_app/apps/predict_api/src/reports/objects.py
@@ -113,6 +113,8 @@ async def create_object_report(id: int):
                         obj.address        as "Адрес потребителя",
                         ecf.description    as "Описание инцидента",
                         ecf.created        as "Дата создания инцидента",
+                        ecf.is_closed  as "Статус инцидента",
+                        ecf.days_of_work  as "Кол-во дней устранения инцидента",
                         ecf.probability    as "Вероятность предсказания",
                         ocs.name           as "Имя ЦТП",
                         ocs.address        as "Адрес ЦТП",
@@ -132,15 +134,55 @@ async def create_object_report(id: int):
                                       ec.description,
                                       ec.created,
                                       ec.probability,
-                                      ec.obj_consumer_id
+                                      ec.obj_consumer_id,
+                                      ec.is_closed,
+                                      ec.days_of_work
                                from event_consumers ec
-                                        join obj_consumers obj on ec.obj_consumer_id = obj.id
-                               where ec.is_closed = false) ecf on ecf.obj_consumer_id = obj.id
+                                        join obj_consumers obj on ec.obj_consumer_id = obj.id) ecf on ecf.obj_consumer_id = obj.id
                          join obj_consumer_stations ocs on obj.obj_consumer_station_id = ocs.id
                     where obj.id = '{str(id)}'
     """
     df_events = pd.read_sql(events_query, sync_db)
+
+    def change_event_status(data):
+        if data:
+            return "Открыт"
+        elif data is None:
+            return '-'
+        elif not data:
+            return "Закрыт"
+
+    df_events["Статус инцидента"] = df_events["Статус инцидента"].apply(lambda x: change_event_status(x))
     df_events["Дата создания инцидента"] = df_events["Дата создания инцидента"].astype(str)
+
+    event_counter_query = f"""
+                select obj.address            as "Адрес потребителя",
+                       ec.contour             as "Контур",
+                       ec.counter_mark        as "Марка счетчика",
+                       ec.counter_number      as "Номер счетчика",
+                       ec.gcal_in_system      as "Объем поданного теплоносителя в систему ЦО",
+                       ec.gcal_out_system     as "Объем обратного теплоносителя в систему ЦО",
+                       ec.subset              as "Разница между подачей и обраткой (Подмес)",
+                       ec.leak                as "Разница между подачей и обраткой (Утечка)",
+                       ec.supply_temp         as "Температура подачи",
+                       ec.return_temp         as "Температура обратки",
+                       ec.work_hours_counter  as "Наработка часов счетчика",
+                       ec.heat_thermal_energy as "Расход тепловой энергии",
+                       ec.errors              as "Ошибка",
+                       ec.errors_desc         as "Описание ошибки",
+                       ec.created             as "Дата создания",
+                       obj.balance_holder     as "Балансодержатель",
+                       obj.type               as "Тип",
+                       obj.priority           as "Приоритет",
+                       obj.energy_class       as "Класс энергоэффективности",
+                       obj.build_year         as "Год постройки",
+                       obj.wear_pct           as "Фактический износ здания, проц."
+                from obj_consumers obj
+                         join event_counters ec on ec.obj_consumer_id = obj.id
+                where obj.id = 2
+                """
+    df_events_counter = pd.read_sql(event_counter_query, sync_db)
+    df_events_counter["Дата создания"] = df_events_counter["Дата создания"].astype(str)
 
     objects_query = f"""
                 select 
@@ -173,9 +215,11 @@ async def create_object_report(id: int):
 
     df_object.to_excel(writer, sheet_name="Потребители", index=False)
     df_events.to_excel(writer, sheet_name="Открытые инциденты", index=False)
+    df_events_counter.to_excel(writer, sheet_name="Выгрузка ОДПУ отопления", index=False)
     df_objects.to_excel(writer, sheet_name="Взаимосвязанные потребители", index=False)
     set_column_size(writer, df_object, "Потребители")
     set_column_size(writer, df_events, "Открытые инциденты")
+    set_column_size(writer, df_events_counter, "Выгрузка ОДПУ отопления")
     set_column_size(writer, df_objects, "Взаимосвязанные потребители")
 
     writer.close()
@@ -185,4 +229,4 @@ async def create_object_report(id: int):
 
 if __name__ == '__main__':
     # asyncio.run(create_objects_report())
-    asyncio.run(create_object_report(10))
+    asyncio.run(create_object_report(3))
