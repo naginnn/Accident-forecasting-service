c241af9b06b52c6196eb082df0a3e1392b989d0c
lestar <gamser11@yandex.ru>
2024-06-15 02:29:36 +0300

    add consumer station report

--- pt_app/apps/predict_api/src/routers.py
+++ pt_app/apps/predict_api/src/routers.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 from apps.predict_api.src.utils import get_recommendations
 from starlette.responses import StreamingResponse
 
-from src.reports.objects import create_objects_report, create_object_report
+from src.reports.objects import create_objects_report, create_object_report, create_consumer_station_report
 
 predict_router = APIRouter(
     tags=["predict"],
@@ -22,6 +22,20 @@ async def predict_model(fields: str, model_name: str = 'default') -> list[dict]:
 @predict_router.get("/objects_report", status_code=200)
 async def get_objects_report():
     file_name, excel_file = await create_objects_report()
+    headers = {
+        'Pragma': 'public',
+        'Expires': '0',
+        'Cache-Control': 'private',
+        'Content-Disposition': f'attachment; filename="{file_name}.xlsx"',
+        'Content-Transfer-Encoding': 'binary',
+        'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    }
+    return StreamingResponse(content=io.BytesIO(excel_file), headers=headers)
+
+
+@predict_router.get("/con_station_report/{id}", status_code=200)
+async def get_consumer_station_report(id: int):
+    file_name, excel_file = await create_consumer_station_report(id)
     headers = {
         'Pragma': 'public',
         'Expires': '0',
